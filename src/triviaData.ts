5df3e3c681edca251d3888820c5651b03c4b7378
Tishtanya Shaw <Tishtanyaa@icloud.com>
2024-03-01 22:19:21 -0500

    Update triviaData

--- src/triviaData.ts
+++ src/triviaData.ts
@@ -4,47 +4,57 @@ export const triviaData = [
 		questions: [
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: ['Artichoke', 'Brick', 'Book', 'Candle'],
 				answer: 'Artichoke',
 			},
 			{
 				question:
 					'I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?',
+				choices: ['Echo', 'Whisper', 'Shadow', 'Cloud'],
 				answer: 'Echo',
 			},
 			{
 				question: "What has keys but can't open locks?",
+				choices: ['Piano', 'Chest', 'Computer', 'Drawer'],
 				answer: 'Piano',
 			},
 			{
 				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Footsteps', 'Memories', 'Breath', 'Shadows'],
 				answer: 'Footsteps',
 			},
 			{
 				question: 'What has cities but no houses, forests, or rivers?',
+				choices: ['Map', 'Globe', 'Book', 'Painting'],
 				answer: 'Map',
 			},
 			{
 				question:
 					'I have cities but no houses, forests but no trees, and rivers but no water. What am I?',
+				choices: ['Globe', 'Planet', 'Universe', 'Country'],
 				answer: 'Globe',
 			},
 			{
 				question:
 					'What comes once in a minute, twice in a moment, but never in a thousand years?',
+				choices: ["The letter 'M'", 'Time', 'Breath', 'Memory'],
 				answer: "The letter 'M'",
 			},
 			{
 				question:
 					'I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?',
+				choices: ['Pencil lead', 'Diamond', 'Gold', 'Coal'],
 				answer: 'Pencil lead',
 			},
 			{
 				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Footsteps', 'Breath', 'Memories', 'Shadows'],
 				answer: 'Footsteps',
 			},
 			{
 				question:
 					'I am always hungry, I must always be fed. The finger I touch, will soon turn red. What am I?',
+				choices: ['Fire', 'Sun', 'Stove', 'Dragon'],
 				answer: 'Fire',
 			},
 		],
@@ -55,28 +65,43 @@ export const triviaData = [
 			{
 				question:
 					'A man gave one son 10 cents and another son was given 15 cents. What time is it?',
+				choices: [
+					"Time to go to bed (sons are given 'cents' to indicate it's bedtime)",
+					'Time to wake up (the man is giving his sons money, indicating morning)',
+					'Time for lunch (the sons are given money for food)',
+					'Time to play (the sons are given money for entertainment)',
+				],
 				answer:
 					"Time to go to bed (sons are given 'cents' to indicate it's bedtime)",
 			},
 			{
 				question:
 					"You see a house with two doors. One door leads to certain death and the other door leads to freedom. There are two guards, one in front of each door. One guard always tells the truth and the other always lies. You don't know which guard is in front of which door. You can ask one guard one question to determine which door leads to freedom. What question do you ask?",
+				choices: [
+					'If I were to ask the other guard which door leads to freedom, what would he say?',
+					'Which door would you choose?',
+					'What is your name?',
+					'What time is it?',
+				],
 				answer:
 					'If I were to ask the other guard which door leads to freedom, what would he say? (Then choose the opposite door)',
 			},
 			{
 				question:
 					'I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?',
+				choices: [],
 				answer: 'Pencil lead',
 			},
 			{
 				question:
 					'A doctor and a bus driver are both in love with the same woman, an attractive girl named Sarah. The bus driver had to go on a long bus trip that would last a week. Before he left, he gave Sarah seven apples. Why?',
+				choices: [],
 				answer: 'An apple a day keeps the doctor away',
 			},
 			{
 				question:
 					"A man is pushing his car along the road when he comes to a hotel. He shouts, 'I'm bankrupt!' Why?",
+				choices: [],
 				answer: 'He was playing Monopoly',
 			},
 			{
@@ -96,6 +121,7 @@ export const triviaData = [
 			},
 			{
 				question: "What has keys but can't open locks?",
+				choices: [],
 				answer: 'Piano',
 			},
 			{
@@ -124,6 +150,7 @@ export const triviaData = [
 			},
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: [],
 				answer: 'Artichoke',
 			},
 			{
@@ -162,12 +189,14 @@ export const triviaData = [
 		category: 'Brain Teasers',
 		questions: [
 			{
-				question: "What has keys but can't open locks?",
-				answer: 'Piano',
+				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Footsteps', 'Memories', 'Breath', 'Shadows'],
+				answer: 'Footsteps',
 			},
 			{
-				question: 'The more you take, the more you leave behind. What am I?',
-				answer: 'Footsteps',
+				question: "What has a heart that doesn't beat?",
+				choices: ['Artichoke', 'Brick', 'Book', 'Candle'],
+				answer: 'Artichoke',
 			},
 			{
 				question:
@@ -195,6 +224,7 @@ export const triviaData = [
 			},
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: [],
 				answer: 'Artichoke',
 			},
 			{
@@ -214,10 +244,12 @@ export const triviaData = [
 			{
 				question:
 					'I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?',
+				choices: ['Pencil lead', 'Gold', 'Coal', 'Diamond'],
 				answer: 'Pencil lead',
 			},
 			{
 				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Footsteps', 'Memories', 'Breath', 'Shadows'],
 				answer: 'Footsteps',
 			},
 			{
@@ -246,6 +278,7 @@ export const triviaData = [
 			},
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: [],
 				answer: 'Artichoke',
 			},
 			{
@@ -265,10 +298,12 @@ export const triviaData = [
 			{
 				question:
 					'What comes once in a minute, twice in a moment, but never in a thousand years?',
+				choices: ["The letter 'M'", 'Time', 'Breath', 'Memory'],
 				answer: "The letter 'M'",
 			},
 			{
 				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Footsteps', 'Memories', 'Breath', 'Shadows'],
 				answer: 'Footsteps',
 			},
 			{
@@ -283,24 +318,29 @@ export const triviaData = [
 			},
 			{
 				question: "What has keys but can't open locks?",
+				choices: ['Guitar', 'Violin', 'Piano', 'Flute'],
 				answer: 'Piano',
 			},
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: ['Apple', 'Artichoke', 'Pineapple', 'Pear'],
 				answer: 'Artichoke',
 			},
 			{
 				question:
 					'I am always hungry, I must always be fed. The finger I touch, will soon turn red. What am I?',
+				choices: ['Cold', 'Ice', 'Fire', 'Hunger'],
 				answer: 'Fire',
 			},
 			{
 				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Memories', 'Time', 'Footsteps', 'Love'],
 				answer: 'Footsteps',
 			},
 			{
 				question:
 					'I have cities but no houses, forests but no trees, and rivers but no water. What am I?',
+				choices: ['Map', 'Globe', 'Dictionary', 'Book'],
 				answer: 'Map',
 			},
 		],
@@ -310,10 +350,12 @@ export const triviaData = [
 		questions: [
 			{
 				question: 'The more you take, the more you leave behind. What am I?',
+				choices: ['Footsteps', 'Memories', 'Breath', 'Shadows'],
 				answer: 'Footsteps',
 			},
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: ['Apple', 'Pineapple', 'Artichoke', 'Pear'],
 				answer: 'Artichoke',
 			},
 			{
@@ -323,34 +365,21 @@ export const triviaData = [
 			},
 			{
 				question: "What has keys but can't open locks?",
+				choices: [],
 				answer: 'Piano',
-			},
-			{
-				question: 'The more you take, the more you leave behind. What am I?',
-				answer: 'Footsteps',
 			},
 			{
 				question:
 					'I am always hungry, I must always be fed. The finger I touch, will soon turn red. What am I?',
+				choices: [],
 				answer: 'Fire',
 			},
-			{
-				question: 'The more you take, the more you leave behind. What am I?',
-				answer: 'Footsteps',
-			},
+
 			{
 				question:
 					'I have cities but no houses, forests but no trees, and rivers but no water. What am I?',
+				choices: [],
 				answer: 'Globe',
-			},
-			{
-				question: 'The more you take, the more you leave behind. What am I?',
-				answer: 'Footsteps',
-			},
-			{
-				question:
-					'I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?',
-				answer: 'Echo',
 			},
 		],
 	},
@@ -360,19 +389,23 @@ export const triviaData = [
 			{
 				question:
 					"I have a long neck, a name of a bird, but I'm not alive. What am I?",
+				choices: ['Flamingo', 'Heron', 'Crane', 'Bottle'],
 				answer: 'A bottle',
 			},
 			{
 				question: 'What has a tail but no body?',
+				choices: ['Snake', 'Lizard', 'Worm', 'Coin'],
 				answer: 'A coin',
 			},
 			{
 				question: "What has a heart that doesn't beat?",
+				choices: ['Apple', 'Pineapple', 'Artichoke', 'Pear'],
 				answer: 'Artichoke',
 			},
 			{
 				question:
 					"I have wings but I'm not a bird. I can swim but I'm not a fish. I can soar through the sky or dive into the sea. What am I?",
+				choices: ['Seagull', 'Penguin', 'Dolphin', 'Bat'],
 				answer: 'A seagull',
 			},
 		],
